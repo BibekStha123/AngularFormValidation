fed71208e2aecd2e5471e07cd2a5e09581181256
bibekstha123 <bibekstha084@gmail.com>
2019-01-09 20:05:21 +0545

    this is my frist angular project.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-second';
+export class AppComponent implements OnInit{
+  
+
+  message: String ="hello";
+
+  constructor(private element : ElementRef){
+
+  }
+
+  ngOnInit(){
+    console.log(this.element.nativeElement);
+  }
+
+
+  onLogin(form : NgForm){
+    console.log(form);
+  }
+
 }
